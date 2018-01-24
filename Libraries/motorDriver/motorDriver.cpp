5d432d70628883b436bbb6c70a3ad3f01e8df430
ameykore <34702262+ameykore@users.noreply.github.com>
2018-01-24 18:32:12 +0530

    Added library for Motor driver

---
+++ Libraries/motorDriver/motorDriver.cpp
@@ -0,0 +1,18 @@
+#include <Wire.h>
+#include "I2C_Anything.h"
+#include "motorDriver.h"
+
+motorDriver::motorDriver(int addr){
+    _addr=addr;
+}
+
+void motorDriver::sendRPM(int rpm) {
+    Wire.beginTransmission (_addr);
+    I2C_writeAnything (rpm);
+    Wire.endTransmission ();
+  }
+void motorDriver::sendPWM(int pwm) {
+    Wire.beginTransmission (_addr);
+    I2C_writeAnything (pwm);
+    Wire.endTransmission ();
+  }
